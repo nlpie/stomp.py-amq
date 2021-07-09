8ab78df9863effcbe54cb3f1abbe7ac0780512f8
Greg Silverman <gms@umn.edu>
2021-07-08 20:42:30 -0500

    stuff

--- stomp_test.py
+++ stomp_test.py
@@ -1,6 +1,8 @@
 # test stomp -> amq
 # see https://stackoverflow.com/questions/9328863/stomp-py-return-message-from-listener/10102673#10102673
 
+# TODO: make class as sper: https://github.com/jasonrbriggs/stomp.py/issues/325
+
 from stomp import *
 from sqlalchemy.engine import create_engine
 from sqlalchemy.sql import text
@@ -31,27 +33,36 @@ lst = MyListener()
 
 # send dtest messages to AMQ
 with engine.connect() as conn:
-    rs = conn.execute('SELECT * FROM notes')
+    #rs = conn.execute('SELECT * FROM notes order by random() limit 1000;')
+    rs = conn.execute('SELECT * FROM notes;')
     for row in rs:
         #print(row.text)
-    	c.send('/queue/test', str({'text':row.text, 'id':row.note_id}), persistent="true")
+        c.send('/queue/test', str({'text':row.text, 'id':row.note_id}), headers={"persistent":"true",
+            "activemq.prefetchSize": 100})
 
 # set listener
 c.set_listener('', lst)
 
 # subscribe client
+#c.subscribe('/queue/test', id=1, ack='client-individual')
 c.subscribe('/queue/test', id=1, ack='client')
+#c.subscribe('/queue/test', id=1, ack='auto')
+
 time.sleep(2)
 messages = lst.msg_list
 
-for m in messages:
-    print(m[0]['message-id'], ast.literal_eval(m[1])['text'],ast.literal_eval(m[1])['id'] )
+message_id = []
 
+for m in messages:
+    #print(m[0]['message-id'], ast.literal_eval(m[1])['text'],ast.literal_eval(m[1])['id'] )
+
+    time.sleep(.5)
     # ack to acknowledge snarfing of message out of queue by message-id
     c.ack(m[0]['message-id'], 1)
+    #messages.remove(m)
+    message_id.append(m[0]['message-id'])
+    print('len', len(message_id))
 
 c.disconnect()
-
-
 
 
